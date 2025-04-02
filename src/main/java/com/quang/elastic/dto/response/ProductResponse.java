36ee027d7d644094c28bda24edf7038e27668795
Quang <quangleminh30062002@gmail.com>
2025-04-02 15:51:22 +0700

    update

--- src/main/java/com/quang/elastic/dto/response/ProductResponse.java
+++ src/main/java/com/quang/elastic/dto/response/ProductResponse.java
@@ -9,7 +9,9 @@ import lombok.*;
 @NoArgsConstructor
 public class ProductResponse {
 
+    private String id;
     private String name;
+    private String description;
     private Double price;
 
 }
